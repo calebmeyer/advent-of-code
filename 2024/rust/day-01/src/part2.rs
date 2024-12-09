90d045e9e5ee609e9284932db32629aeaff41d90
Caleb Meyer <Kiaulen@gmail.com>
2024-12-08 22:20:58 -0600

    start day 1

--- 2024/rust/day-01/src/part2.rs
+++ 2024/rust/day-01/src/part2.rs
@@ -1,30 +1,6 @@
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String> {
-    let mut left = vec![];
-    let mut right = vec![];
-
-    for line in input.lines() {
-        let mut items = line.split_whitespace();
-        left.push(
-            items.next().unwrap().parse::<usize>().unwrap(),
-        );
-        right.push(
-            items.next().unwrap().parse::<usize>().unwrap(),
-        );
-    }
-
-    let result: usize = left
-        .iter()
-        .map(|number| {
-            number
-                * right
-                    .iter()
-                    .filter(|r| &number == r)
-                    .count()
-        })
-        .sum();
-
-    Ok(result.to_string())
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day 01 - part 2");
 }
 
 #[cfg(test)]
@@ -33,13 +9,9 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
-        assert_eq!("31", process(input)?);
+        todo!("haven't built test yet");
+        let input = "";
+        assert_eq!("", process(input)?);
         Ok(())
     }
 }
